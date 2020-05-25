a00d1a3a35b9e50d094f914b40ad12640031e810
Parham Alvani <parham.alvani@gmail.com>
2020-05-25 23:01:35 +0430

    feat; Return Iterations Result in Dictionary

--- benders.py
+++ benders.py
@@ -179,26 +179,42 @@ class SubProblem:
 
 
 def main():
+    iterations = {
+        "k": [],
+        "x_1": [],
+        "x_2": [],
+        "x_3": [],
+        "z_lb": [],
+        "z_ub": [],
+        "pi_1": [],
+        "pi_2": [],
+        "pi_3": [],
+    }
+
     master_problem = MasterProblem()
-    iterations = 0
+    k = 0
 
     while True:
-        print()
-        print(f"> {iterations}")
-        iterations += 1
+        k += 1
+        iterations["k"].append(k)
 
         z_lb, x_1, x_2, x_3 = master_problem.solve()
-        print(f"x_1: {x_1}, x_2: {x_2}, x_3: {x_3}", f"z_lb: {z_lb}")
+
+        iterations["x_1"].append(x_1)
+        iterations["x_2"].append(x_2)
+        iterations["x_3"].append(x_3)
+        iterations["z_lb"].append(z_lb)
 
         z_star, pi_1, pi_2, pi_3 = SubProblem().solve(x_1, x_2, x_3)
         z_ub = z_star + WHEAT_COST * x_1 + CORN_COST * x_2 + BEET_COST * x_3
-        print(f"pi_1: {pi_1}, pi_2: {pi_2}, pi_3: {pi_3}", f"z_ub: {z_ub}")
+
+        iterations["pi_1"].append(pi_1)
+        iterations["pi_2"].append(pi_2)
+        iterations["pi_3"].append(pi_3)
+        iterations["z_ub"].append(z_ub)
 
         if math.isclose(z_lb, z_ub, abs_tol=EPSILON):
-            print()
-            print(f"x_1: {x_1}, x_2: {x_2}, x_3: {x_3}")
-            print(f"Z: {z_lb}")
-            break
+            return iterations
 
         master_problem.add_cut(
             z_star - pi_1 * x_1 - pi_2 * x_2 - pi_3 * x_3, pi_1, pi_2, pi_3
